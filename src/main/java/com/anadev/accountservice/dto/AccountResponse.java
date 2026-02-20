5edc3097521d7a64d2008cbf71246308798b1f56
Ana Carolina Cordeiro Aguilar <ana.aguilar@agi.com.br>
2026-02-20 16:42:26 -0300

    remove classe que utilizava streambridge desnecessario

--- src/main/java/com/anadev/accountservice/dto/AccountResponse.java
+++ src/main/java/com/anadev/accountservice/dto/AccountResponse.java
@@ -1,6 +1,7 @@
 package com.anadev.accountservice.dto;
 
 import com.anadev.accountservice.entity.Account;
+import com.anadev.accountservice.entity.User;
 import com.anadev.accountservice.entity.enums.TypeAccount;
 import com.anadev.accountservice.entity.enums.TypeCurrency;
 
@@ -11,6 +12,7 @@ import java.math.BigDecimal;
 public record AccountResponse(
         Long idAccount,
         String name,
+        User user,
         TypeAccount typeAccount,
         TypeCurrency currency,
         BigDecimal currentBalance,
@@ -20,6 +22,7 @@ public record AccountResponse(
         return new AccountResponse(
                 account.getIdAccount(),
                 account.getName(),
+                account.getUser(),
                 account.getTypeAccount(),
                 account.getCurrency(),
                 account.getCurrentBalance(),
